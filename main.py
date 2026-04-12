4db91bfa2f40775a6babc71c538b38571c15cf70
TaskinOkmen <taskin.okmen@gmail.com>
2026-04-12 16:36:59 +0300

    stable version realtime run with azimuth printing

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import numpy as np
+import sounddevice as sd
 import matplotlib.pyplot as plt
+from scipy.io import wavfile
+import queue
+import sys
+
+from matplotlib.animation import FuncAnimation
 
 # Constant Parameters
-fs_hz = 44100                      # Sampling frequency
+fs_hz = 16000                      # Sampling frequency
 f_hz = 1000                        # Sine frequency (1 kHz)
-duration_s = 0.02                  # 20 ms signal
+duration_s = 0.1                  # 20 ms signal
 
 
 # (2.915451895044e-4)   -> gives 0   degrees
@@ -21,6 +27,32 @@ DISTANCE_BETWEEN_MICS_M = 10e-2 # 10 cm
 
 DEGREES_PER_RADIAN = 57.2957795 # degrees
 
+BLOCK_SIZE = 4096 # 8192 2048 4096
+HOP_SIZE   = 1080  #  320 480  1080
+
+
+def estimate_aoa_over_time_overlap_to_array(mic_left_signal, mic_right_signal, fs_hz, mic_distance_m, block_size=BLOCK_SIZE, hop_size=HOP_SIZE):
+
+    num_samples = len(mic_left_signal)
+    angles = []
+
+    for start in range(0, num_samples - block_size, hop_size):
+
+        end = start + block_size
+
+        block_x1 = mic_left_signal[start:end]
+        block_x2 = mic_right_signal[start:end]
+
+        angle = calculate_tdoa_rad(
+            block_x1,
+            block_x2,
+            fs_hz,
+            mic_distance_m
+        )
+
+        angles.append(angle * DEGREES_PER_RADIAN)
+
+    return np.array(angles)
 
 
 def calculate_tdoa_rad(mic_left_signal, mic_right_signal, fs_hz, mic_distance_m):
@@ -42,7 +74,7 @@ def calculate_time_difference_s(mic_left_signal, mic_right_signal, fs_hz):
     R = X1 * np.conj(X2)
 
     # Partial PHAT normalization (power -0.3)
-    R_phat = R / (np.abs(R) ** (-0.3) + 1e-10)
+    R_phat = R / ((np.abs(R) + 1e-10) ** (-0.3) + 1e-10)
 
     # --- IFFT -> correlation ---
     gcc = np.fft.ifft(R_phat)
@@ -68,40 +100,193 @@ def calculate_angle_of_arrival_rad(time_difference_s, mic_distance_m):
 
     val = np.clip((time_difference_s * SPEED_OF_SOUND_METERS_PER_SECOND) / mic_distance_m, -1.0, 1.0)
 
-    return np.arccos(val)
+    return np.arcsin(val)
+
+
+#  buffers
+BUFFER_SIZE = BLOCK_SIZE * 2
+
+buffer_left = np.zeros(BUFFER_SIZE)
+buffer_right = np.zeros(BUFFER_SIZE)
+
+q = queue.Queue()
+
+def estimate_aoa_over_time_overlap(x1, x2, block_size, hop_size):
+
+    num_samples = BUFFER_SIZE
+    angles = []
+
+    for start in range(0, num_samples - block_size, hop_size):
+
+        end = start + block_size
+
+        block_x1 = x1[start:end]
+        block_x2 = x2[start:end]
+
+        angle = calculate_tdoa_rad(
+            block_x1,
+            block_x2,
+            fs_hz,
+            DISTANCE_BETWEEN_MICS_M
+        )
+
+        #angles.append(angle)
+        print(angle * DEGREES_PER_RADIAN)
 
 
 
+def audio_callback(indata, frames, time, status):
+    global buffer_left, buffer_right
 
-if __name__ == '__main__':
+    # assume channel 1 = left, 2 = right
+    new_left = indata[:, 1]
+    new_right = indata[:, 2]
 
-    t = np.arange(0, duration_s, 1/fs_hz)
+    buffer_left[BUFFER_SIZE//2 : BUFFER_SIZE//2 + frames] = new_left
+    buffer_right[BUFFER_SIZE//2 : BUFFER_SIZE//2 + frames] = new_right
 
-    # Original signal
-    mic_right_signal = np.random.randn(len(t))
+    estimate_aoa_over_time_overlap(buffer_left, buffer_right, BLOCK_SIZE, HOP_SIZE)
 
-    # Delayed signal
-    mic_left_signal = np.concatenate((np.zeros(delay_samples), mic_right_signal[:-delay_samples]))
+    # shift buffer (FIFO)
+    np.copyto(buffer_left[:BLOCK_SIZE], buffer_left[BLOCK_SIZE:])
+    np.copyto(buffer_right[:BLOCK_SIZE], buffer_right[BLOCK_SIZE:])
+
+
+
+# def update_plot(frame):
+#     """This is called by matplotlib for each plot update.
+
+#     Typically, audio callbacks happen more frequently than plot updates,
+#     therefore the queue tends to contain multiple blocks of audio data.
+
+#     """
+#     global plotdata
+#     while True:
+#         try:
+#             data = q.get_nowait()
+#         except queue.Empty:
+#             break
+#         shift = len(data)
+#         plotdata = np.roll(plotdata, -shift, axis=0)
+#         plotdata[-shift:, :] = data
+#     for column, line in enumerate(lines):
+#         line.set_ydata(plotdata[:, column])
+#     return lines
+
+
+# try:
+#     if args.samplerate is None:
+#         device_info = sd.query_devices(args.device, 'input')
+#         args.samplerate = device_info['default_samplerate']
+
+#     length = int(args.window * args.samplerate / (1000 * args.downsample))
+#     plotdata = np.zeros((length, len(args.channels)))
+
+#     fig, ax = plt.subplots()
+#     lines = ax.plot(plotdata)
+#     ax.axis((0, len(plotdata), -1, 1))
+#     ax.set_yticks([0])
+#     ax.yaxis.grid(True)
+#     ax.tick_params(bottom=False, top=False, labelbottom=False,
+#                    right=False, left=False, labelleft=False)
+#     fig.tight_layout(pad=0)
+
+#     stream = sd.InputStream(
+#         device=args.device, channels=max(args.channels),
+#         samplerate=args.samplerate, callback=audio_callback)
+#     ani = FuncAnimation(fig, update_plot, interval=args.interval, blit=True)
+#     with stream:
+#         plt.show()
+# except Exception as e:
+#     pass
+
+
+
+# start stream
+with sd.InputStream(
+    samplerate=fs_hz,
+    channels=8,           # your RASP-LC has 8 channels
+    dtype='int16',
+    blocksize=BLOCK_SIZE,
+    callback=audio_callback
+):
+    print("Listening... Press Ctrl+C to stop")
+    while True:
+        pass
+
+# if __name__ == '__main__':
+
+#     sr, mic_right_signal = wavfile.read("recording_ch2.wav")
+ 
+#     sr, mic_left_signal = wavfile.read("recording_ch1.wav")
+
+#     angles = estimate_aoa_over_time_overlap_to_array(mic_left_signal, mic_right_signal, fs_hz, DISTANCE_BETWEEN_MICS_M)
+
+#     print(angles)
     
-    # plt.subplot(2, 1, 1)
-    # plt.plot(t, mic_left_signal, label = 'Mic Left', marker='o', color = 'orange')
-    # plt.legend()
-    # plt.grid()
+#     plt.plot(angles, label = 'Mic Right', marker='o')
+#     plt.legend()
 
-    # plt.subplot(2, 1, 2)
-    # plt.plot(t, mic_right_signal, label = 'Mic Right', marker='o')
-    # plt.legend()
-
-    # plt.grid()
-    # plt.show()
+#     plt.grid()
+#     plt.show()
 
 
-    time_difference_s = calculate_time_difference_s(mic_left_signal, mic_right_signal, fs_hz)
 
 
-    print("Estimated delay (seconds):", time_difference_s)
-    print("Estimated delay (ms):", time_difference_s * 1000)
+# TEST CODE BELOW - NOT USED IN FINAL VERSION
 
-    angle_of_arrival_rad = calculate_tdoa_rad(mic_left_signal, mic_right_signal, fs_hz, DISTANCE_BETWEEN_MICS_M)
-    print("Angle of Arrival (rads):", angle_of_arrival_rad)
-    print("Angle of Arrival (degrees):", angle_of_arrival_rad * DEGREES_PER_RADIAN)
+# angles = []
+
+# def test_estimate_aoa_over_time_overlap(x1, x2, block_size, hop_size):
+
+#     num_samples = BUFFER_SIZE
+    
+
+#     for start in range(0, num_samples - block_size, hop_size):
+
+#         end = start + block_size
+
+#         block_x1 = x1[start:end]
+#         block_x2 = x2[start:end]
+
+#         angle = calculate_tdoa_rad(
+#             block_x1,
+#             block_x2,
+#             fs_hz,
+#             DISTANCE_BETWEEN_MICS_M
+#         )
+
+#         angles.append(angle * DEGREES_PER_RADIAN)
+#         #print(angle * DEGREES_PER_RADIAN)
+
+# def test_audio_callback(left_signal, right_signal, frames):
+#     global buffer_left, buffer_right
+
+#     # assume channel 1 = left, 2 = right
+#     new_left = left_signal
+#     new_right = right_signal
+
+#     buffer_left[BUFFER_SIZE//2 : BUFFER_SIZE//2 + frames] = new_left
+#     buffer_right[BUFFER_SIZE//2 : BUFFER_SIZE//2 + frames] = new_right
+
+#     test_estimate_aoa_over_time_overlap(buffer_left, buffer_right, BLOCK_SIZE, HOP_SIZE)
+
+#     # shift buffer (FIFO)
+#     np.copyto(buffer_left[:BLOCK_SIZE], buffer_left[BLOCK_SIZE:])
+#     np.copyto(buffer_right[:BLOCK_SIZE], buffer_right[BLOCK_SIZE:])
+
+# if __name__ == '__main__':
+
+#     sr, mic_right_signal = wavfile.read("recording_ch2.wav")
+ 
+#     sr, mic_left_signal = wavfile.read("recording_ch1.wav")
+
+
+#     for i in range(0, len(mic_left_signal) - BLOCK_SIZE, HOP_SIZE):
+#         test_audio_callback(mic_left_signal[i:i+BLOCK_SIZE], mic_right_signal[i:i+BLOCK_SIZE], BLOCK_SIZE)
+    
+#     plt.plot(angles, label = 'Mic Right', marker='o')
+#     plt.legend()
+
+#     plt.grid()
+#     plt.show()
